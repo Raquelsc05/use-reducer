4dae8cc0d46cc07cf0082c14ef4dc16f2ee4390d
Raquel <raquelcosta5@gmail.com>
2024-06-20 18:34:06 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useReducer, useState } from 'react';
 import './App.css';
+import reducer, { ADICIONAR_FRASE, EXCLUIR_FRASE } from './Reducer.js';
+
+
 
 function App() {
+  const [frase, setFrase] = useState('')
+  const [frases, dispatch] = useReducer(reducer, [])
+
+  function salvarFrase(evento) {
+    evento.preventDefault()
+    dispatch({
+      tipo:ADICIONAR_FRASE,
+      frase
+
+    })
+
+  }
+
+   function excluir(fraseExcluida){
+    dispatch({
+      tipo:EXCLUIR_FRASE,
+      frase: fraseExcluida
+    })
+   }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={salvarFrase}>
+     <textarea 
+     value={frase}
+     onChange={evento => setFrase(evento.target.value)}
+     placeholder='Digite sua frase...'
+     required />
+        <br />
+      <button>Salvar frase</button>
+    </form>
+    {frases.map((fraseAtual, index) => <p key={index}>{fraseAtual} - <button onClick={() => excluir(fraseAtual)}>excluir</button></p>)}
     </div>
   );
 }
---
+++ src/Reducer.js
@@ -0,0 +1,25 @@
+export const ADICIONAR_FRASE = 'ADICIONAR_FRASE'
+export const EXCLUIR_FRASE = 'EXCLUIR_FRASE'
+
+const reducer = (estado, acao) => {
+  switch (acao.tipo) {
+    case ADICIONAR_FRASE:
+      if (acao.frase.length < 20) {
+        alert('Ops...Nao sao permitidadas frases com menos de 20 caracteres!')
+        return estado
+      }
+      if (estado.includes(acao.frase)) {
+        alert('Nao sao permitidas frases duplicadas!')
+        return estado
+      }
+      return [...estado, acao.frase]
+
+    case EXCLUIR_FRASE:
+      return estado.filter(frase => frase !== acao.frase)
+
+    default:
+      return estado
+  }
+}
+
+export default reducer
